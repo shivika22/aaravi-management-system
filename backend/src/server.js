88634852abd805b95da40791bec4416992fb39e7
Shivika Jain <shivik@Shivikas-MacBook-Air.local>
2026-07-13 00:48:43 +0530

    Fix Render portcommand

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 import app from "./app.js";
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
